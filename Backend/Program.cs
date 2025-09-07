ea5a5661c53e45a823fc6aaed775128c1c32c8da
anton <antonrumancev67@gmail.com>
2025-09-07 22:42:49 +0300

    added swagger auth

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -109,6 +109,30 @@ builder.Services.AddAuthentication(options => {
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "Coworkify", Version = "v1" });
+    
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            []
+        }
+    });
 });
 
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
